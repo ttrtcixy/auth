11ef2a4b75bc02feca0f8e1fcaa76602c6e99776
ttrtcixy <ttrtcixy@gmail.com>
2026-01-21 00:03:36 +0400

    feat: add conversion domain transactions to driver transactions

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -4,7 +4,9 @@ import (
 	"context"
 	"log/slog"
 
+	"github.com/jackc/pgx/v5"
 	storage "github.com/ttrtcixy/pgx-wrapper"
+	"github.com/ttrtcixy/users/internal/domain"
 	authrepo "github.com/ttrtcixy/users/internal/repository/auth"
 )
 
@@ -20,10 +22,32 @@ func NewRepository(ctx context.Context, log *slog.Logger, db *storage.Postgres) 
 	}
 }
 
-func (r *Repository) RunInTx(ctx context.Context, fn func(context.Context) error) error {
-	err := r.DB.RunInTx(ctx, fn)
-	if err != nil {
-		return err
+func (r *Repository) RunInTx(ctx context.Context, txOptions domain.TxOptions, fn func(context.Context) error) error {
+	var isoLevel pgx.TxIsoLevel
+	switch txOptions.IsoLevel {
+	case domain.Serializable:
+		isoLevel = pgx.Serializable
+	case domain.ReadCommitted:
+		isoLevel = pgx.ReadCommitted
+	case domain.RepeatableRead:
+		isoLevel = pgx.RepeatableRead
+	case domain.ReadUncommitted:
+		isoLevel = pgx.ReadUncommitted
+	default:
+		r.log.LogAttrs(ctx, slog.LevelWarn, "transaction isolation level not specified, set by default: ReadCommitted")
+		isoLevel = pgx.ReadCommitted
 	}
-	return nil
+
+	var accessMode pgx.TxAccessMode
+
+	switch txOptions.AccessMode {
+	case domain.ReadWrite:
+		accessMode = pgx.ReadWrite
+	case domain.ReadOnly:
+		accessMode = pgx.ReadOnly
+	default:
+		accessMode = pgx.ReadWrite
+	}
+
+	return r.DB.RunInTx(ctx, pgx.TxOptions{IsoLevel: isoLevel, AccessMode: accessMode}, fn)
 }
